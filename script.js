cb43a9bc3017165282ffbab8010fe2e52091254f
Hoang Phuong Nam <hoan0090@algonquinlive.com>
2020-01-12 21:23:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = "Nam"
+console.log(`Hello, my name is ${name}`)
+
+var number = "10"
+var result1 = number*2
+var result2 = result1*5
+var result3 = result2/number
+var result4 = result3-7
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+
